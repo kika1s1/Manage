c78dbe50dcf625f2b362cf42c4f16802fa917a7d
kika1s1 <tamiratkebede120@gmail.com>
2024-07-02 06:39:12 +0300

    testimonials is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Testimonials from "./components/Testimonials";
 const App = () => (
   <>
     <Header/>
     <Hero/>
     <Features/>
+    <Testimonials/>
   </>
 );
 export default App;
