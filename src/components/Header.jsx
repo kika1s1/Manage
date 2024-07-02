a838feda6e8c671c794f98728143749fe28a6877
kika1s1 <tamiratkebede120@gmail.com>
2024-07-02 05:55:22 +0300

    navbar is finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,35 @@
-
-
 const Header = () => {
   return (
     <nav className="relative container p-6 mx-auto">
-        <div className="flex items-center justify-between">
-            {/* logo */}
-            <div className="pt-2">
-                <img src="logo.svg" alt="" />
-            </div>
-            <div className="hidden md:flex space-x-6">
-                <a href="#">Pricing</a>
-                <a href="#">About Us</a>
-                <a href="#">Services</a>
-                <a href="#">Community</a>
-
-            </div>
-
+      <div className="flex items-center justify-between">
+        {/* logo */}
+        <div className="pt-2">
+          <img src="logo.svg" alt="" />
         </div>
-        
+        <div className="hidden md:flex space-x-6">
+          <a className="hover:text-darkGrayishBlue" href="#">
+            Pricing
+          </a>
+          <a className="hover:text-darkGrayishBlue" href="#">
+            About Us
+          </a>
+          <a className="hover:text-darkGrayishBlue" href="#">
+            Services
+          </a>
+          <a className="hover:text-darkGrayishBlue" href="#">
+            Community
+          </a>
+        </div>
+        {/* button */}
+        <a
+          className=" hidden md:block baseline hover:bg-brightRedLight text-white rounded-full px-6 p-3 pt-2 bg-brightRed"
+          href="#"
+        >
+          Get Started
+        </a>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
