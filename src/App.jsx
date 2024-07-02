fc172065e7b6da194e2141c55545288ee3702fb0
kika1s1 <tamiratkebede120@gmail.com>
2024-07-02 06:30:16 +0300

    features is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 const App = () => (
   <>
     <Header/>
     <Hero/>
+    <Features/>
   </>
 );
 export default App;
