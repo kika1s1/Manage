7057bc13832438bf964ea8b124584e6a63441eb8
kika1s1 <tamiratkebede120@gmail.com>
2024-07-02 06:50:47 +0300

    cta is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Testimonials from "./components/Testimonials";
+import CTA from "./components/CTA";
 const App = () => (
   <>
     <Header/>
     <Hero/>
     <Features/>
     <Testimonials/>
+    <CTA/>
   </>
 );
 export default App;
