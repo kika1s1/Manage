a2bbbe40bfdf774485d13d6815dc04b639fc4a69
kika1s1 <tamiratkebede120@gmail.com>
2024-07-02 07:13:27 +0300

    humber menu is added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Testimonials from "./components/Testimonials";
 import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 const App = () => (
   <>
     <Header/>
@@ -10,6 +11,7 @@ const App = () => (
     <Features/>
     <Testimonials/>
     <CTA/>
+    <Footer/>
   </>
 );
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Use the current state value directly
+  };
+
   return (
     <nav className="relative container p-6 mx-auto">
       <div className="flex items-center justify-between">
@@ -27,6 +35,29 @@ const Header = () => {
         >
           Get Started
         </a>
+        {/* <!-- Hamburger Icon --> */}
+        <button
+          onClick={toggleMenu}
+          id="menu-btn"
+          className="block hamburger md:hidden focus:outline-none"
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
+      </div>
+      {/* <!-- Mobile Menu --> */}
+      <div className="md:hidden">
+        <div
+          id="menu"
+          className={`absolute ${isOpen ? "flex" : "hidden"}  flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}
+        >
+          <a href="#">Pricing</a>
+          <a href="#">Product</a>
+          <a href="#">About Us</a>
+          <a href="#">Careers</a>
+          <a href="#">Community</a>
+        </div>
       </div>
     </nav>
   );
